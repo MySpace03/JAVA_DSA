352d6510ed484a3169671aae020ceda9d5b287d9
MySpace03 <120674982+MySpace03@users.noreply.github.com>
2023-02-05 15:52:38 +0530

    Add files via upload

---
+++ Sorting/Bubblesort.java
@@ -0,0 +1,33 @@
+package DSA.Sorting;
+import java.util.*;
+public class Bubblesort {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the arry: ");
+        int len = sc.nextInt();
+        System.out.print("Enter the element of the array: ");
+        int arr [] = new int[len];
+        for(int i = 0 ; i<len; i++){
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+        System.out.print("sorted arry: ");
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+    static void bubbleSort(int arr[]){
+        int len = arr.length;
+        for (int i=0;i<len-1;i++){
+            for(int j=0;j<len-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    
+}
